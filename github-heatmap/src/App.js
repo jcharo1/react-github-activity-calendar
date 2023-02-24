9ba631e867bfcabc2753620d5dce1287f3ea4763
Justin Charo <65956091+jcharo1@users.noreply.github.com>
2023-02-24 10:03:36 -0600

    added styling to  graph weeks

--- github-heatmap/src/App.js
+++ github-heatmap/src/App.js
@@ -1,6 +1,6 @@
 // Import everything needed to use the `useQuery` hook
 import { useQuery, gql, useMutation } from "@apollo/client";
-
+import Graph from "./Graph";
 const GET_LOCATIONS = gql`
   query ($userName: String!) {
     user(login: $userName) {
@@ -24,38 +24,38 @@ const GET_LOCATIONS = gql`
 //   --url 'https://flyby-router-demo.herokuapp.com/' \
 //   --data '{"query":"query GetLocations { locations {id name description photo}}"}'
 
-function DisplayLocations() {
-  //   const [gitAct] = useMutation(GET_LOCATIONS);
-  //   console.log(gitAct);
-  const { loading, error, data } = useQuery(GET_LOCATIONS, {
-    variables: { userName: "jcharo1" },
-  });
-  //   const {data, error,} =  gitAct({
-  //     "variables":
-  //     { "userName": "jcharo1"}}
-  //   )
+// function DisplayLocations() {
+//   //   const [gitAct] = useMutation(GET_LOCATIONS);
+//   //   console.log(gitAct);
+//   const { loading, error, data } = useQuery(GET_LOCATIONS, {
+//     variables: { userName: "jcharo1" },
+//   });
+//   //   const {data, error,} =  gitAct({
+//   //     "variables":
+//   //     { "userName": "jcharo1"}}
+//   //   )
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error : {error.message}</p>;
 
-  return data.locations.map(({ id, name, description, photo }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
-    </div>
-  ));
-}
+//   return data.locations.map(({ id, name, description, photo }) => (
+//     <div key={id}>
+//       <h3>{name}</h3>
+//       <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+//       <br />
+//       <b>About this location:</b>
+//       <p>{description}</p>
+//       <br />
+//     </div>
+//   ));
+// }
 
 export default function App() {
   return (
     <div>
-      <h2>My first Apollo app 🚀</h2>
-      <br />
-      <DisplayLocations />
+      <Graph />
+
+      {/* <DisplayLocations /> */}
     </div>
   );
 }
