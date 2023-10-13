b0e0582f34fdb207c0748bc6e6bd9c6d7b7ad69a
Justin Charo <65956091+jcharo1@users.noreply.github.com>
2023-10-13 01:17:14 -0500

    updated width

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@
 import Graph from "./lib/components/Graph";
 
 export default function App() {
-  const githubApiKey = ""; // <your-github-api-key>
-  const githubUserName = ""; // <github-username>
+  const githubApiKey = "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"; // <your-github-api-key>
+  const githubUserName = "jcharo1"; // <github-username>
   const bgcolor = "";
   const textColor = "";
 
