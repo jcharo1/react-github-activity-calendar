f3a8124c4e4c4b56612e672ebb918d3054941d45
Justin Charo <65956091+jcharo1@users.noreply.github.com>
2024-09-27 00:13:45 -0500

    conditionally apply styles

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
         githubApiKey={githubApiKey}
         backgroundColor={bgcolor}
         color={textColor}
+        enableAnimations={false}
       />
     </div>
   );
--- src/lib/components/Graph.js
+++ src/lib/components/Graph.js
@@ -11,6 +11,15 @@ import {
 // components/Graph.js
 
 const Graph = (props) => {
+  // Destructure props for easier access and set default for enableAnimations
+  const {
+    userName,
+    githubApiKey,
+    color,
+    backgroundColor,
+    enableAnimations = true, // Default to true if not provided
+  } = props;
+
   // Initialize weeks with initial data
   const [weeks, setWeeks] = useState(createInitialWeeks());
   const [hoveredDayIndex, setHoveredDayIndex] = useState(null);
@@ -22,20 +31,14 @@ const Graph = (props) => {
   useEffect(() => {
     const getData = async () => {
       try {
-        // Fetch contributions by year (ensure this function returns the needed data)
-        const rest = await getAllContributionsByYear(
-          props.userName,
-          props.githubApiKey
-        );
-        console.log("🚀 ~ getData ~ rest:", rest);
-
+        // Fetch contributions by year
+        // const rest = await getAllContributionsByYear(userName, githubApiKey);
         // Fetch data from GitHub GraphQL API
         const response = await fetch(
           "https://api.github.com/graphql",
-          requestOptions(props.userName, props.githubApiKey)
+          requestOptions(userName, githubApiKey)
         );
         const result = await response.json();
-        console.log("🚀 ~ getData ~ result:", result);
 
         // Extract weeks and total contributions from the response
         const contributionCalendar =
@@ -50,10 +53,10 @@ const Graph = (props) => {
       }
     };
 
-    if (props.githubApiKey && props.userName) {
+    if (githubApiKey && userName) {
       getData();
     }
-  }, [props.githubApiKey, props.userName]);
+  }, [githubApiKey, userName]);
 
   const isNeighbor = (currentWeekIndex, currentDayIndex) => {
     const weekDiff = Math.abs(currentWeekIndex - hoveredWeekIndex);
@@ -64,26 +67,24 @@ const Graph = (props) => {
     // Exclude if both weekDiff and dayDiff are equal to 2 (corners)
     return weekDiff <= 2 && dayDiff <= 2 && !(weekDiff === 2 && dayDiff === 2);
   };
+
   // Define style options based on week index
   const getStyleOptions = (count) => {
     if (count <= 5) {
       return {
-        backgroundColor: props.color || "",
-        color: props.backgroundColor || "",
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 75%, 5% 77%, 0 100%, 0 78%)",
+        backgroundColor: color || "",
+        color: backgroundColor || "",
         transform: "translate(0%, 0%) scaleY(1)",
       };
     } else if (count <= 48) {
       return {
-        backgroundColor: props.color || "",
-        color: props.backgroundColor || "",
+        backgroundColor: color || "",
+        color: backgroundColor || "",
       };
     } else {
       return {
-        backgroundColor: props.color || "",
-        color: props.backgroundColor || "",
-        clipPath:
-          "polygon(0% 0%, 100% 0%, 100% 75%, 100% 74%, 100% 100%, 94% 77%, 0 76%)",
+        backgroundColor: color || "",
+        color: backgroundColor || "",
         transform: "translate(-100%, 0%) scaleY(1)",
       };
     }
@@ -113,40 +114,59 @@ const Graph = (props) => {
         dayCount === 0 ? "react-github-activity-calendar-remove-outline" : "";
       const bgColorClass = getColor(dayCount);
 
+      // Conditionally add animation-related classes
+      const dayClassNames = [
+        "react-github-activity-calendar-calender-day",
+        loading
+          ? "react-github-activity-calendar-calender-day-loading shimmer"
+          : enableAnimations
+          ? "react-github-activity-calendar-calender-day-transition"
+          : "",
+        bgColorClass,
+        bgOutline,
+        enableAnimations &&
+        isNeighbor(weekIndex, dayIndex) &&
+        hoveredDayIndex !== null
+          ? "react-github-activity-calendar-bg-day-scale-down"
+          : "",
+      ]
+        .filter(Boolean)
+        .join(" ");
+
+      // Conditionally apply event handlers
+      const eventHandlers = enableAnimations
+        ? {
+            onMouseEnter: () => {
+              setHoveredDayIndex(dayIndex);
+              setHoveredWeekIndex(weekIndex);
+            },
+            onMouseLeave: () => {
+              setHoveredDayIndex(null);
+              setHoveredWeekIndex(null);
+            },
+          }
+        : {};
+
+      // Conditionally apply styles
+      const dayStyles = enableAnimations
+        ? {
+            "--animation-delay": `${(dayIndex % 7) * 0.1 + weekIndex * 0.1}s`,
+            transitionDelay: `${(dayIndex % 7) * 0.07}s`,
+          }
+        : {};
+
       return (
         <div
           key={`day-${weekIndex}-${dayIndex}`}
-          className={`react-github-activity-calendar-calender-day ${
-            loading
-              ? "react-github-activity-calendar-calender-day-loading  shimmer"
-              : " react-github-activity-calendar-calender-day-transition"
-          } ${bgColorClass} ${bgOutline} ${
-            isNeighbor(weekIndex, dayIndex) && hoveredDayIndex !== null
-              ? "react-github-activity-calendar-bg-day-scale-down"
-              : ""
-          }`}
-          onMouseEnter={() => {
-            setHoveredDayIndex(dayIndex);
-            setHoveredWeekIndex(weekIndex);
-          }}
-          onMouseLeave={() => {
-            setHoveredDayIndex(null);
-            setHoveredWeekIndex(null);
-          }}
-          style={{
-            "--animation-delay": `${(dayIndex % 7) * 0.1 + weekIndex * 0.1}s`,
-            // transition: "transform 0.3s, box-shadow 0.3s",
-            transitionDelay: `${(dayIndex % 7) * 0.07}s`,
-          }}
+          className={dayClassNames}
+          {...eventHandlers}
+          style={dayStyles}
         >
           <div
             className="react-github-activity-calendar-calender-day__text-container"
             style={styleOptionsDay}
           >
-            <div
-              className="react-github-activity-calendar-calender-day__text"
-              // style={styleOptionsDay}
-            >
+            <div className="react-github-activity-calendar-calender-day__text">
               {dayCount}&nbsp;contributions on {DayName[formattedDayName]}
               ,&nbsp;
               {months[month]} {formattedDay}, {formattedYear}
@@ -156,25 +176,26 @@ const Graph = (props) => {
       );
     });
 
+    // Conditionally attach hover event handlers to weeks
+    const weekEventHandlers = enableAnimations
+      ? {
+          onMouseEnter: () => {
+            setWeekIndexHovered(weekIndex);
+          },
+          onMouseLeave: () => {
+            setWeekIndexHovered(null);
+          },
+        }
+      : {};
+
     return (
       <React.Fragment key={`week-${weekIndex}`}>
-        {/* {displayMonth && ( */}
-        <div className="react-github-activity-calendar-calender-week__month ">
+        <div className="react-github-activity-calendar-calender-week__month">
           {displayMonth}
         </div>
-        {/* /* )} */}
         <div
-          // style={{
-          //   backgroundColor:
-          //     Math.abs(weekIndex - weekIndexHovered) <= 3 ? "red" : "yellow",
-          // }}
-          onMouseEnter={() => {
-            setWeekIndexHovered(weekIndex);
-          }}
-          onMouseLeave={() => {
-            setWeekIndexHovered(null);
-          }}
           className="react-github-activity-calendar-calender-week"
+          {...weekEventHandlers}
         >
           {weekDays}
         </div>
@@ -186,13 +207,13 @@ const Graph = (props) => {
     <div
       className="react-github-activity-calendar-container"
       style={{
-        backgroundColor: props.backgroundColor || "",
-        color: props.color || "",
+        backgroundColor: backgroundColor || "",
+        color: color || "",
       }}
     >
       <a
         className="react-github-activity-calendar-github-mark first-column"
-        href={`https://github.com/${props.userName}`}
+        href={`https://github.com/${userName}`}
       >
         {/* GitHub SVG Icon */}
         <svg
@@ -219,10 +240,10 @@ const Graph = (props) => {
 
       {calendarWeeks}
 
-      <div className="react-github-activity-calendar-bottom-row ">
+      <div className="react-github-activity-calendar-bottom-row">
         <a
           className="react-github-activity-calendar-bottom-row__link first-column"
-          href={`https://github.com/${props.userName}`}
+          href={`https://github.com/${userName}`}
         >
           <span>{totalContributions} contributions in the last year&nbsp;</span>
         </a>
@@ -241,201 +262,3 @@ const Graph = (props) => {
 };
 
 export default Graph;
-
-// const Graph = (props) => {
-//   // Initialize the state with initial weeks
-//   const [apicall, setApiCall] = useState({
-//     user: {
-//       contributionsCollection: {
-//         contributionCalendar: {
-//           weeks: createInitialWeeks(),
-//         },
-//       },
-//     },
-//   });
-//   const [loading, setLoading] = useState(true); // Start with loading=true
-
-//   useEffect(() => {
-//     const getData = async () => {
-//       try {
-//         const rest = await getAllContributionsByYear(
-//           props.userName,
-//           props.githubApiKey
-//         );
-//         console.log("🚀 ~ getData ~ rest:", rest);
-
-//         const response = await fetch(
-//           "https://api.github.com/graphql",
-//           requestOptions(props.userName, props.githubApiKey)
-//         );
-//         const result = await response.json();
-//         console.log(result);
-
-//         setApiCall(result.data);
-//         setLoading(false); // Data has been loaded
-//       } catch (e) {
-//         console.error("Error fetching data:", e);
-//         setLoading(false); // Even if there's an error, stop loading
-//       }
-//     };
-
-//     if (props.githubApiKey && props.userName) {
-//       getData();
-//     }
-//   }, [props.githubApiKey, props.userName]);
-
-//   // Determine which data to use: real data or initial weeks
-//   const formatData = loading
-//     ? apicall.user.contributionsCollection.contributionCalendar
-//     : {
-//         weeks: createInitialWeeks(),
-//       };
-
-//   let count = 0;
-//   let styleOptionsDay;
-
-//   const calendarWeeks = formatData?.weeks.map((week, weekIndex) => {
-//     let displayMonth;
-//     count += 1;
-//     if (count <= 5) {
-//       styleOptionsDay = {
-//         backgroundColor: props.color || "",
-//         color: props.backgroundColor || "",
-//         clipPath: "polygon(0% 0%, 100% 0%, 100% 75%, 5% 77%, 0 100%, 0 78%)",
-//         transform: "translate(0%, 0%) scaleY(1)",
-//       };
-//     } else if (count <= 48) {
-//       styleOptionsDay = {
-//         backgroundColor: props.color || "",
-//         color: props.backgroundColor || "",
-//       };
-//     } else {
-//       styleOptionsDay = {
-//         backgroundColor: props.color || "",
-//         color: props.backgroundColor || "",
-//         clipPath:
-//           "polygon(0% 0%, 100% 0%, 100% 75%, 100% 74%, 100% 100%, 94% 77%, 0 76%)",
-//         transform: "translate(-100%, 0%) scaleY(1)",
-//       };
-//     }
-
-//     const weekDays = week.contributionDays.map((day, dayIndex) => {
-//       const dayCount = day.contributionCount;
-//       const dayDate = new Date(day.date);
-
-//       const formattedYear = dayDate.getFullYear();
-//       const formattedDay = dayDate.getDate();
-//       const formattedDayName = dayDate.getDay();
-//       let month = dayDate.getMonth();
-
-//       if (formattedDay === 1) {
-//         displayMonth = months[month];
-//       }
-//       let bgOutline =
-//         dayCount === 0 ? "react-github-activity-calendar-remove-outline" : "";
-//       let bgColorClass = getColor(dayCount);
-
-//       return (
-//         <div
-//           key={`day-${weekIndex}-${dayIndex}`}
-//           className={`react-github-activity-calendar-calender-day ${bgColorClass} ${bgOutline}`}
-//         >
-//           <div
-//             className="react-github-activity-calendar-calender-day__text-container"
-//             style={styleOptionsDay}
-//           >
-//             <div className="react-github-activity-calendar-calender-day__text">
-//               {dayCount}&nbsp;contributions on {DayName[formattedDayName]}
-//               ,&nbsp;
-//               {months[month]} {formattedDay}, {formattedYear}
-//             </div>
-//           </div>
-//         </div>
-//       );
-//     });
-
-//     return (
-//       <React.Fragment key={`week-${weekIndex}`}>
-//         <div className="react-github-activity-calendar-calender-week__month">
-//           {displayMonth}
-//         </div>
-
-//         <div className="react-github-activity-calendar-calender-week">
-//           {weekDays}
-//         </div>
-//       </React.Fragment>
-//     );
-//   });
-
-//   return (
-//     <div
-//       className="react-github-activity-calendar-container"
-//       style={{
-//         backgroundColor: props.backgroundColor || "",
-//         color: props.color || "",
-//       }}
-//     >
-//       <a
-//         className="react-github-activity-calendar-github-mark"
-//         href={`https://github.com/${props.userName}`}
-//       >
-//         {/* GitHub SVG Icon */}
-//         <svg
-//           viewBox="0 0 100 100"
-//           width="12px"
-//           height="12px"
-//           xmlns="http://www.w3.org/2000/svg"
-//         >
-//           <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" />
-//         </svg>
-//       </a>
-//       <div className="react-github-activity-calendar-calender-day-label">
-//         <div className="react-github-activity-calendar-calender-day-label__name">
-//           Mon
-//         </div>
-//         <div className="react-github-activity-calendar-calender-day-label__name">
-//           Wed
-//         </div>
-
-//         <div className="react-github-activity-calendar-calender-day-label__name">
-//           Fri
-//         </div>
-//       </div>
-
-//       {calendarWeeks}
-
-//       <div className="react-github-activity-calendar-bottom-row">
-//         <a
-//           className="react-github-activity-calendar-bottom-row__link"
-//           href={`https://github.com/${props.userName}`}
-//         >
-//           <span>
-//             {formatData?.totalContributions || "0"} contributions in the last
-//             year&nbsp;
-//           </span>
-//         </a>
-//         <div className="react-github-activity-calendar-less-more-container">
-//           <span> Less </span>
-//           <div
-//             className={`react-github-activity-calendar-calender-day react-github-activity-calendar-bg-day react-github-activity-calendar-example-size `}
-//           ></div>
-//           <div
-//             className={`react-github-activity-calendar-calender-day react-github-activity-calendar-bg-day--L1 react-github-activity-calendar-example-size `}
-//           ></div>
-//           <div
-//             className={`react-github-activity-calendar-calender-day react-github-activity-calendar-bg-day--L2 react-github-activity-calendar-example-size `}
-//           ></div>
-//           <div
-//             className={`react-github-activity-calendar-calender-day react-github-activity-calendar-bg-day--L3 react-github-activity-calendar-example-size `}
-//           ></div>
-//           <div
-//             className={`react-github-activity-calendar-calender-day react-github-activity-calendar-bg-day--L4 react-github-activity-calendar-example-size `}
-//           ></div>
-//           <span> More </span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Graph;
